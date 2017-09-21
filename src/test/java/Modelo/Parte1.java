882d5a4fdfeb6b6e1e9caebb870d589f55b1c0e5
BrianS01 <brian.sterling@correo.usa.edu.co>
2017-09-21 15:33:38 -0500

    05. Adaptacion
    Union con procesos anteriores, antigua seccion.

--- src/test/java/Modelo/Parte1.java
+++ src/test/java/Modelo/Parte1.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package parte.pkg1;
+package Modelo;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
