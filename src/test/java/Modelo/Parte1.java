de81b2e6df9648a4ff8e3f7d5b68c3226962c81d
BrianS01 <brian.sterling@correo.usa.edu.co>
2017-09-21 15:30:23 -0500

    05. Adaptacion
    Union con procesos anteriores, antigua seccion.

---
+++ src/test/java/Modelo/Parte1.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package parte.pkg1;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
+/**
+ *
+ * @author Mario-Bx
+ */
+public class Parte1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+        // TODO code application logic here
+      
+//        //pruebas de cliente
+//        Cliente Cli = new Cliente();
+//        Cli.crearArchio();
+//        Cli.crearCliente("Mario Bolaños Roa", "crr 8G Bis 159B-62", "3212942930", 5, "12/09/2017", 5565390);
+//        Cli.crearCliente("Brian S. Sterling", "crr 15 75B-62", "3234529320", 5, "12/02/2016", 20560780);
+//        System.out.println(Cli.leerCleinte(2));
+//        Cli.actualizarCliente(2, "Nombre", "Andre Felipe Peralta");
+//        System.out.println(Cli.leerCleinte(2));
+//        Cli.borrarCliente(2);
+//        Cli.existe(2);
+        
+        
+        
+//        //Prueba vendeodor
+//        Verdedor Ven = new Verdedor();
+//        Ven.crearArchivo();
+//        Ven.crearVendedor("1020345876", "Sergio Orozco", "crr 170 cll 67-89", "3132077809");
+//        Ven.crearVendedor("1029765396", "Juan Albarracin", "crr 170 cll 67-89", "3132077809");
+//        System.out.println(Ven.leerVendedor(1));
+//        Ven.actualizarVendedodr(2, "Nombre", "Andre Felipe Peralta");
+//        System.out.println(Ven.leerVendedor(2));
+//        Ven.borrarVendedor(2);
+//        Ven.existe(2);
+        
+        
+        
+        
+//        //prueba Pedido
+//        Pedido Ped = new Pedido();
+//        Ped.crearArchivo();
+//        Ped.crearPedido("whatasApp", 4.6, 0.13, 0.01, "Blindado", 3, 1);
+//        Ped.crearPedido("Telefonica", 6.4, 2, 0.5, "Reflectivo", 1, 2);
+//        System.out.println(Ped.leerPedido(1));
+//        Ped.actualizarPedido(1, "Cantidad", "500");
+//        System.out.println(Ped.leerPedido(1));
+//        Ped.borrarPedido(1);
+//        Ped.existe(1);
+        
+        
+        
+//        Producto Pro = new Producto();
+//        Pro.crearArchivo();
+//        Pro.crearProducto(4.6, 2.8, 0.5, "larga duracion", "Blindado");
+//        Pro.crearProducto(6.4, 8.2, 2, "Periodo de Prueba", "Brillante");
+//        System.out.println(Pro.leerProducto(1));
+//        Pro.actualizarProducto(2, "Calidad", "Indestructible");
+//        System.out.println(Pro.leerProducto(2));
+//        Pro.borrarProducto(2);
+//        Pro.existe(2);
+        
+        
+//        Factura Fac = new Factura();
+//        Fac.crearArchivo();
+//        Fac.crearFactura("12/09/2017", 5678987, 1, 2, 1);
+//        Fac.crearFactura("17/12/2015", 9876544, 2, 1, 2);
+//        System.out.println(Fac.leerDactura(2));
+//        Fac.actualizarFactura(2, "Valor", "111111");
+//        System.out.println(Fac.leerDactura(2));
+//        Fac.borrarFactura(2);
+//        Fac.existe(2);
+        
+        
+    }
+    
+}
